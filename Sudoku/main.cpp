95909c36fac7d0ab2b6acfdef23ce81f67e18030
Adham Ali <30821115+AdhamAliAhmed@users.noreply.github.com>
2020-03-25 21:00:15 +0200

    Add project files.

---
+++ Sudoku/main.cpp
@@ -0,0 +1,637 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include<string>
+
+using namespace sf;
+using namespace std;
+
+namespace sudoku
+{
+	/**
+	* Time formatting and manipulation
+	*
+	*/
+	struct timer {
+		sf::Clock mC;
+		float runTime;
+		bool bPaused;
+		timer() {
+			bPaused = false;
+			runTime = 0;
+			mC.restart();
+		}
+		void Reset() {
+			mC.restart();
+			runTime = 0;
+			bPaused = false;
+		}
+		void Resume() {
+			if (bPaused) {
+				mC.restart();
+			}
+			bPaused = false;
+		}
+		void Pause() {
+			if (!bPaused) {
+				runTime += mC.getElapsedTime().asSeconds();
+			}
+			bPaused = true;
+		}
+		float GetElapsedSeconds() {
+			if (!bPaused) {
+				return runTime + mC.getElapsedTime().asSeconds();
+			}
+			return runTime;
+		}
+		string getElapsedTime()
+		{
+			int seconds = int(GetElapsedSeconds());
+			int minutes = seconds / 60;
+			int hours = minutes / 60;
+			minutes = (seconds - hours * 3600) / 60;
+			seconds -= (minutes * 60 + hours * 3600);
+
+			string time;
+			if (hours > 1)
+				time = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+			else
+				time = to_string(minutes) + ":" + to_string(seconds);
+
+			return time;
+		}
+	};
+
+
+	/**
+	* Stores the data of a move during the game for later use
+	*
+	*/
+	struct gameMove
+	{
+		Vector2i index;
+		int number = 0;
+	};
+
+	/**
+	* Controls the process of drawing both pen and numbers on the grid and figuring out the index of the cell which the pen refers to
+	*
+	*/
+	struct positionSystem
+	{
+		const Vector2f NUMBERS_MARGIN = { 41,125 };
+		const Vector2f POINTER_MARGIN = { 32,128 };
+		const Vector2f ICON_MARGIN = { 57,47 };
+
+		Vector2f margin, factor, position;
+		Vector2i index;
+
+		Vector2f generate_position()
+		{
+			float x = margin.x + (index.x * factor.x),
+				y = margin.y + (index.y * factor.y);
+
+			position = { x,y };
+			return position;
+		}
+
+		Vector2i generate_index()
+		{
+			int x = (position.x - margin.x) / factor.x,
+				y = (position.y - margin.y) / factor.y;
+
+			index = { x,y };
+			return index;
+		}
+
+	};
+
+}
+
+
+#pragma region GLOBAL VARIABLES
+const int MOVE_SLEEP_DURATION = 175;
+
+
+int totalMoves = 0; //total moves taken in the game
+int correctMoves = 0; //correct moves taken in the game
+int falseMoves = 0; //false moves taken, (game will be over after 3 false moves)
+
+//Used to trace the pen in order to move it using the keyboard arrows and knowing the current position 
+Vector2i penTracer = { 0,0 };
+
+int completedCells = 41;
+
+sudoku::gameMove lastMove;
+sudoku::timer timer;
+bool paused = false;
+VideoMode videoMode(500, 650);
+RenderWindow window(videoMode, "SUDOKU", Style::Close);
+#pragma endregion
+
+
+/**
+* Evaluates if a move is correct or not comparing to the completed sudoku template
+*
+* @param index The index of the cell.
+* @param source The reference to check whether a correct move or not.
+* @param num The number played.
+*
+* @return true if the cell is either empty or contains a false number from a previous move
+*/
+bool correctMove(Vector2i index, int source[9][9], int num)
+{
+	return source[index.x][index.y] == num;
+}
+
+/**
+* Evaluates either a specific cell is modifiable or not
+*
+* @param index The index of the cell.
+* @param solvedSource The solved sudoku grid.
+* @param unsolvedSource The unsolved sudoku grid.
+*
+* @return true if the cell is modifiable
+*/
+bool modifiableCell(Vector2i index, int solvedSource[9][9], int unsolvedSource[9][9])
+{
+	bool emptyCell = unsolvedSource[index.x][index.y] == 0;
+	bool wrongNumber = !correctMove(index, solvedSource, solvedSource[index.x][index.y]);
+
+	return emptyCell || wrongNumber;
+}
+
+/**
+* Removes an entry entered by the used
+*
+* @param source The reference grid in which we are going to remove the entry
+* @param index The index of the entry
+*
+*/
+void remove(Text source[9][9], Vector2i index)
+{
+	source[index.x][index.y].setString("0");
+}
+
+/**
+* Undo the last move taken
+*
+* @param source The reference grid in which we are going to remove the last move
+* @param move The move to be removed
+*
+*/
+void undo(Text source[9][9], sudoku::gameMove move)
+{
+	remove(source, move.index);
+}
+
+/**
+* Plays a sound from a sound buffer then awaits for a specific time
+*
+* @param name The name of the audio file
+* @param sleepDuration the duration to be awaited
+*/
+void playSound(string name, Time sleepDuration)
+{
+	string directory = "Assets/Audio/SoundEffects/" + name;
+	SoundBuffer buffer;
+	buffer.loadFromFile(directory);
+
+	Sound sound(buffer);
+	sound.play();
+
+	sleep(sleepDuration);
+}
+
+/**
+* Updates a text object with a specific string
+*
+* @param textObj The text object to be updates
+* @param initialString Data that does not change each frame
+* @param newData Data that does change each frame
+*/
+void updateText(Text& textObj, string initialString, string newData)
+{
+	string str = initialString + newData;
+	textObj.setString(str);
+}
+
+int main()
+{
+	int unsolvedTemplate[9][9] =
+	{
+		{1,0,3,0,0,0,0,0,0},
+		{0,2,0,4,5,6,0,8,9},
+		{1,2,3,4,0,6,7,0,0},
+		{1,0,3,0,0,0,0,8,9},
+		{0,2,3,4,5,6,0,0,0},
+		{1,2,0,4,0,6,0,8,9},
+		{0,2,3,0,5,0,7,0,0},
+		{1,0,0,0,5,0,0,0,9},
+		{1,2,3,0,0,6,0,8,0}
+	};
+
+	int solvedTemplate[9][9] =
+	{
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9},
+		{1,2,3,4,5,6,7,8,9}
+	};
+
+#pragma region Containers
+	//Loading textures
+	Texture textureOptions;
+	textureOptions.loadFromFile("Assets/Images/options.png");
+
+	Texture textureGrid;
+	textureGrid.loadFromFile("Assets/Images/grid.png");
+
+	//assigning textures and positioning sprites
+	Sprite spriteOptions(textureOptions);
+	spriteOptions.setPosition(25, 25);
+
+	Sprite spriteGrid(textureGrid);
+	spriteGrid.setPosition(25, 120);
+#pragma endregion
+
+#pragma region Typography
+	float typographyScale = 0.7f,
+		positionY = 580.0f;
+
+	//Fonts (Product Sans)
+	Font googleBlack, googleBold, googleRegular;
+	googleBlack.loadFromFile("Assets/Fonts/ProductSans-Black.ttf");
+	googleBold.loadFromFile("Assets/Fonts/ProductSans-Bold.ttf");
+	googleRegular.loadFromFile("Assets/Fonts/ProductSans-Regular.ttf");
+
+
+	Text unsolvedTemplateText[9][9]; //Sudoku numbers to be drawn
+
+	string difficulty; //Game difficulty, (Easy, Normal, Hard, Expert)
+	Text difficultyText; //Game difficulty
+	difficultyText.setString("NORMAL");
+	difficultyText.setFont(googleRegular);
+	difficultyText.setScale({ typographyScale,typographyScale });
+	difficultyText.setPosition(25, positionY);
+
+	Text falseMovesText;
+	falseMovesText.setFont(googleBold);
+	falseMovesText.setFillColor(Color(255, 128, 128));
+	falseMovesText.setScale({ typographyScale,typographyScale });
+	falseMovesText.setPosition(160, positionY);
+
+	Text timeText; //Game time
+	timeText.setString("TIME: 01:24");
+	timeText.setFont(googleRegular);
+	timeText.setScale({ typographyScale,typographyScale });
+	timeText.setPosition(380, positionY);
+#pragma endregion
+
+	/*************************
+	*******SUDOKU PEN*********
+	**************************/
+	RectangleShape pen({ 35,35 });
+	//Pen styling
+	pen.setFillColor(Color(0, 0, 0)); //transparent fill color
+	pen.setOutlineColor(Color::White);
+	pen.setOutlineThickness(3);
+
+	/************************************************************************
+	*******COPYING THE UNSOLVED ARRAY IN ORDER TO DRAW IT EACH FRAME*********
+	*************************************************************************/
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			int num = unsolvedTemplate[i][j];
+			//setting string
+			unsolvedTemplateText[i][j].setString(to_string(num));
+			//setting font (Google Sans)
+			unsolvedTemplateText[i][j].setFont(googleBlack);
+			//setting position
+			sudoku::positionSystem posNumber;
+			posNumber.margin = posNumber.NUMBERS_MARGIN;
+			posNumber.factor = { 50,50 };
+			posNumber.index = { i,j };
+			Vector2f pos = posNumber.generate_position();
+
+			unsolvedTemplateText[i][j].setPosition(pos);
+		}
+	}
+
+	//Game loop
+	while (window.isOpen())
+	{
+		//Closing the game
+		Event evnt{};
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+
+			case Event::LostFocus:
+				paused = true;
+				timer.Pause();
+				break;
+			case Event::GainedFocus:
+				paused = false;
+				timer.Resume();
+				break;
+			default: break;
+			}
+		}
+
+		/************************
+		*****UPDATING LABELS*****
+		*************************/
+#pragma region UPDATING LABELS
+
+		updateText(falseMovesText, "FALSE MOVES: ", to_string(falseMoves));
+
+		//keep updating the time unless the game is paused
+		if (!paused)
+			updateText(timeText, "TIME: ", timer.getElapsedTime());
+#pragma endregion
+
+
+		/************************************************
+		 ****************KEYBOARD INPUT******************
+		 ***********************************************/
+
+
+		if (!paused)
+		{
+#pragma region KEYBOARD INPUT
+
+			/********************************************************
+			 *****HANDLING ARROWS INPUT IN ORDER TO MOVE THE PEN*****
+			 *******************************************************/
+#pragma region PEN MOVING INPUT
+			bool rightPressed = Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D),
+				leftPressed = Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A),
+				upPressed = Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W),
+				downPressed = Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S),
+				directionPressed = rightPressed | leftPressed | upPressed | downPressed;
+
+			if (directionPressed)
+			{
+				//right arrow
+				if (rightPressed)
+				{
+					//moves the pen to the start of the grid if the pen reaches the end of the grid
+					if (penTracer.x == 8 && penTracer.y == 8)
+					{
+						penTracer.x = 0;
+						penTracer.y = 0;
+					}
+					//moves the pen to the next row if the pen reaches the end of the current row
+					else if (penTracer.x == 8)
+					{
+						penTracer.x = 0;
+						penTracer.y++;
+					}
+					//moves the pen horizontally towards the right
+					else
+					{
+						penTracer.x++;
+					}
+				}
+				//left arrow
+				if (leftPressed)
+				{
+					//moves the pen to the end of the grid if the pen reaches the start of the grid
+					if (penTracer.x == 0 && penTracer.y == 0)
+					{
+						penTracer.x = 8;
+						penTracer.y = 8;
+					}
+					//moves the pen to the previous row if the pen reaches the start of the current row
+					else if (penTracer.x == 0)
+					{
+						penTracer.x = 8;
+						penTracer.y--;
+					}
+					//moves the pen horizontally towards the left
+					else
+					{
+						penTracer.x--;
+					}
+				}
+				//up arrow
+				if (upPressed)
+				{
+					//Does nothing if the pen is already at the start of the column
+					if (penTracer.y == 0)
+					{
+					}
+					//moving up the column of the grid
+					else
+					{
+						penTracer.y--;
+					}
+				}
+				//down arrow
+				if (downPressed)
+				{
+					//Does nothing if the pen is already at the end of the column
+					if (penTracer.y == 8)
+					{
+					}
+					//moving up the column of the grid
+					else
+					{
+						penTracer.y++;
+					}
+				}
+
+				//sound playing when moving the pen
+				playSound("move.wav", milliseconds(MOVE_SLEEP_DURATION));
+			}
+
+			//Setting pen position
+			sudoku::positionSystem posPen;
+			posPen.margin = posPen.POINTER_MARGIN;
+			posPen.factor = { 50,50 };
+			posPen.index = { penTracer.x, penTracer.y };
+			Vector2f pos = posPen.generate_position();
+			pen.setPosition(pos);
+#pragma endregion
+
+			/********************************************************
+			 ********HANDLING NUMBERS INPUT IN ORDER TO WRITE********
+			 *******************************************************/
+#pragma region NUMBERS INPUT
+			int numPressed = 0;
+
+			if (Keyboard::isKeyPressed(Keyboard::Num1) || Keyboard::isKeyPressed(Keyboard::Numpad1))
+				numPressed = 1;
+			if (Keyboard::isKeyPressed(Keyboard::Num2) || Keyboard::isKeyPressed(Keyboard::Numpad2))
+				numPressed = 2;
+			if (Keyboard::isKeyPressed(Keyboard::Num3) || Keyboard::isKeyPressed(Keyboard::Numpad3))
+				numPressed = 3;
+			if (Keyboard::isKeyPressed(Keyboard::Num4) || Keyboard::isKeyPressed(Keyboard::Numpad4))
+				numPressed = 4;
+			if (Keyboard::isKeyPressed(Keyboard::Num5) || Keyboard::isKeyPressed(Keyboard::Numpad5))
+				numPressed = 5;
+			if (Keyboard::isKeyPressed(Keyboard::Num6) || Keyboard::isKeyPressed(Keyboard::Numpad6))
+				numPressed = 6;
+			if (Keyboard::isKeyPressed(Keyboard::Num7) || Keyboard::isKeyPressed(Keyboard::Numpad7))
+				numPressed = 7;
+			if (Keyboard::isKeyPressed(Keyboard::Num8) || Keyboard::isKeyPressed(Keyboard::Numpad8))
+				numPressed = 8;
+			if (Keyboard::isKeyPressed(Keyboard::Num9) || Keyboard::isKeyPressed(Keyboard::Numpad9))
+				numPressed = 9;
+
+			//The actual writing and playing sound
+
+			if (numPressed != 0) //A number between (1-9) entered
+			{
+				Vector2i index = penTracer;
+
+				if (modifiableCell(index, solvedTemplate, unsolvedTemplate))
+				{
+					unsolvedTemplateText[index.x][index.y].setString(to_string(numPressed));
+					unsolvedTemplateText[index.x][index.y].setFont(googleRegular);
+
+					//coloring the number to indicate either correct or incorrect move
+					if (correctMove(index, solvedTemplate, numPressed))
+					{
+						unsolvedTemplateText[index.x][index.y].setFillColor(Color(0, 0, 180));
+						completedCells++;
+					}
+					else
+					{
+						unsolvedTemplateText[index.x][index.y].setFillColor(Color(255, 0, 0));
+						falseMoves++;
+					}
+
+					//record the move in case the user wants to undo
+					lastMove.index = index;
+					lastMove.number = numPressed;
+
+					//playing sound
+					playSound("fill.wav", milliseconds(350));
+				}
+			}
+#pragma endregion
+
+
+			/********************************************************
+			 ********************DELETING A MOVE*********************
+			 *******************************************************/
+#pragma region 4-DELETE
+			if (Keyboard::isKeyPressed(Keyboard::Delete)) //user pressed delete in order to remove a specific move
+			{
+				//Doesn't delete the predefined numbers
+				if (modifiableCell(penTracer, solvedTemplate, unsolvedTemplate))
+				{
+					remove(unsolvedTemplateText, penTracer);
+
+					//plays the undo sound
+					playSound("undo.wav", milliseconds(250));
+					//decrease completed cells 
+				}
+			}
+#pragma endregion
+			/********************************************************
+			 ********************UN-DOING A MOVE*********************
+			 *******************************************************/
+#pragma region 5-UNDO
+			if (Keyboard::isKeyPressed(Keyboard::Z) && evnt.key.control) //user pressed ctrl+z in order to undo the last move
+			{
+				//solves the issue of removing number at (0,0) at the beginning of the game
+				if (modifiableCell(lastMove.index, solvedTemplate, unsolvedTemplate))
+				{
+					undo(unsolvedTemplateText, lastMove);
+
+					//plays the undo sound
+					playSound("undo.wav", milliseconds(250));
+
+
+					//decrease completed cells 
+				}
+			}
+#pragma endregion
+#pragma endregion
+		}
+
+		/********************************************************
+		 *******UPDATING AND SWITCHING BUFFERS*******************
+		 *******************************************************/
+#pragma region UPDATING AND SWITCHING BUFFERS
+
+		 //clearing the window
+		window.clear();
+
+		//drawing sprites
+		RectangleShape background({ 500, 650 });
+		background.setFillColor(Color(50, 50, 50));
+		window.draw(background);
+
+		window.draw(spriteOptions);
+		window.draw(spriteGrid);
+
+		//Drawing components
+		//Pen
+		window.draw(pen);
+		//Numbers
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (unsolvedTemplateText[j][i].getString() == "0")
+					continue;
+				window.draw(unsolvedTemplateText[j][i]);
+			}
+
+		}
+
+		//texts
+		window.draw(timeText);
+		window.draw(falseMovesText);
+		window.draw(difficultyText);
+
+		//Draw an overlay shader to indicate a paused game
+		if (paused)
+		{
+			RectangleShape pauseShade({ static_cast<float>(textureGrid.getSize().x), static_cast<float>(textureGrid.getSize().y) });
+			pauseShade.setPosition({ 25, 120 });
+			pauseShade.setFillColor(Color(50, 50, 50, 240));
+
+			Texture texturePause;
+			texturePause.loadFromFile("Assets/pause.png");
+			Sprite spritePause;
+			spritePause.setTexture(texturePause);
+			spritePause.setOrigin(spritePause.getGlobalBounds().width / 2, spritePause.getGlobalBounds().height / 2);
+			spritePause.setPosition(window.getSize().x / 2.0f, window.getSize().y / 3.0f);
+
+			Text pauseText;
+			pauseText.setString("PAUSED");
+			pauseText.setFont(googleBlack);
+			pauseText.setFillColor(Color::White);
+			pauseText.setCharacterSize(84);
+			pauseText.setOrigin(pauseText.getGlobalBounds().width / 2, pauseText.getGlobalBounds().height / 2);
+			pauseText.setPosition(window.getSize().x / 2.0f, spritePause.getPosition().y + spritePause.getGlobalBounds().height);
+
+			window.draw(pauseShade);
+			window.draw(spritePause);
+			window.draw(pauseText);
+		}
+
+		window.display();
+
+#pragma endregion
+	}
+
+
+	return 0;
+}
